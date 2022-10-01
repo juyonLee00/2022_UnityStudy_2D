db0daa9e9229f72ed6e0ad968ce351990b8cab20
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2022-10-01 22:53:23 +0900

    타일맵 구현

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed;
+    public float jumpPower;
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
+    Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if(Input.GetButtonUp("Horizontal"))
+        if (Input.GetButtonDown("Jump") && !anim.GetBool("IsJumping"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("IsJumping", true);
+        }
+
+        if (Input.GetButtonUp("Horizontal"))
         {
             rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
         }
@@ -25,16 +34,35 @@ public class PlayerMove : MonoBehaviour
         {
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
         }
+
+        if (Mathf.Abs(rigid.velocity.x) < 0.3)
+            anim.SetBool("IsWalking", false);
+        else
+            anim.SetBool("IsWalking", true);
     }
 
     void FixedUpdate()
     {
         float h = Input.GetAxisRaw("Horizontal");
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+        rigid.AddForce(Vector2.right * 2 * h, ForceMode2D.Impulse);
 
         if (rigid.velocity.x > maxSpeed)
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
         else if (rigid.velocity.x < maxSpeed*(-1))
             rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+
+        if(rigid.velocity.y < 0)
+        {
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance < 0.5f)
+                    anim.SetBool("IsJumping", false);
+            }
+        }
+
+        
     }
 }
